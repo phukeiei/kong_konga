0ebaae264483cff26eada97bc0875044df119063
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-11-25 17:15:34 +0700

    version02
    version02

--- proxy-service/routes/index.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const indexController = require("./../controllers/index.comtroller");
-
-router.get("/", indexController.index);
-router.get("/test", indexController.test);
-
-module.exports = router;
